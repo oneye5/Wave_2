98743f6a16748b4162de69ba712d7c231bda7489
oneye5 <ocjlaz@gmail.com>
2022-12-16 22:44:58 +1300

    Initial commit

---
+++ Assets/Scripts/Netcode/HighLevel_Netcode.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
+using UnityEngine;
+
+public class HighLevelNetcode : MonoBehaviour
+{
+    public const string KEY_JOINCODE = "RELAYKEY";
+    public const string KEY_MAP= "MAP";
+    public const string KEY_MODE = "MAP";
+
+    const int defaultPlayerCount = 8;
+
+
+    bool inGame = false;
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+    //high level stuffs
+    public void QuickJoin()
+    {
+
+    }
+    public async Task<bool> JoinGame(Lobby l)
+    {
+        var joinCode = l.Data[KEY_JOINCODE].Value;
+        l = await joinLobby(l);
+        await joinRelay(joinCode);
+        NetworkManager.Singleton.StartClient();
+
+        return true;
+    }
+    public async void CreateGame(int maxPlayers,string lobbyname,int map,int mode)
+    {
+        var joinCode = await createRelay();
+        await createLobby(joinCode);
+        NetworkManager.Singleton.StartHost();
+    }
+    public async void QuickCreateGame()
+    {
+         var joinCode = await createRelay();
+        await createLobby(joinCode);
+        NetworkManager.Singleton.StartHost();
+    }
+
+
+
+    // relay system
+    public async Task<String> createRelay() //returns the join code
+    {
+        try
+        {
+            var allocation = await RelayService.Instance.CreateAllocationAsync(defaultPlayerCount);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log(joinCode);
+
+
+            //crreate connection
+            var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            var relayData = new RelayServerData(allocation , "dtls");
+            transport.SetRelayServerData(relayData);
+            return joinCode;
+        }
+        catch(RelayServiceException e)
+        {
+            Debug.Log(e);
+            return null;
+        }
+    }
+    public async Task joinRelay(string joinCode)
+    {
+        try
+        {
+            var allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            var relayData = new RelayServerData(allocation , "dtls");
+            transport.SetRelayServerData(relayData);
+        }
+        catch(RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+    //lobby system
+    public async Task<Lobby> joinLobby(Lobby l)
+    {
+        try
+        {
+            var x = await Lobbies.Instance.JoinLobbyByIdAsync(l.Id);
+            Debug.Log("joined " + x.Name + " max players " + x.MaxPlayers + " code " + x.LobbyCode);
+            return x;
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.LogError(e);
+            return null;
+        }
+    }
+    public async Task<Lobby> createLobby(string relayCode , int maxPlayers =defaultPlayerCount, string lobbyName = "defaultLobbyName",int map =0,int mode= 0)
+    {
+        try
+        {
+            CreateLobbyOptions options = new CreateLobbyOptions();
+            //  options.Player = //to impliment
+            options.IsPrivate = false;
+            options.Data = new Dictionary<string , DataObject>
+            {
+                {KEY_JOINCODE,new DataObject(DataObject.VisibilityOptions.Member,relayCode) },
+                {KEY_MAP,new DataObject(DataObject.VisibilityOptions.Public,map.ToString()) },
+                   {KEY_MODE,new DataObject(DataObject.VisibilityOptions.Public,mode.ToString()) },
+            };
+
+
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName , maxPlayers , options);
+
+            Debug.Log("lobby created " + lobbyName + " " + maxPlayers);
+            return lobby;
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.LogError(e);
+            return null;
+        }
+    }
+
+}
